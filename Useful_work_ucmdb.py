d9d88450be84371e5ea0e448d786ffc28ef9a93b
Кувалин Влад <59653793+Vkuvalin@users.noreply.github.com>
2022-09-20 22:08:52 +0300

    Update Useful_work_ucmdb.py

--- Useful_work_ucmdb.py
+++ Useful_work_ucmdb.py
@@ -1,5 +1,115 @@
 p = None
 
+"----------------------Передача словаря значений между функциями----------------------------"
+# def _createCCIpAddress(ip, num, OSHVResult, description=None, ca_global_id=None, ca_model=None, ca_node_os_name=None,
+#                        ca_name=None, ca_serial=None, ca_sm_id=None, ca_node_os_vendor=None, ca_primary_dns_name=None,
+#                        ca_device_type=None, ca_firmware=None, ca_rack=None, site=None, ca_vendor=None,
+#                        ca_bucket=None, ca_config=None, ca_datacenter=None, ca_dcroom=None,
+#                        ca_interface=None, ca_location=None, ca_ids=None):
+#     print(ca_device_type)
+#     print(ca_bucket)
+#     print(ca_interface)
+#     print(ca_config)
+#     print(ca_location)
+#
+#
+# def incomingDataProcessing(ip, num, OSHVResult, **fufufu):
+#     _createCCIpAddress(ip, num, OSHVResult, **fufufu)
+#
+#
+# ca_device_type = '1'
+# ca_bucket = '2'
+# ca_config = '3'
+# ca_interface = '4'
+# ca_location = '5'
+#
+# incomingDataProcessing('IP', 'NUM', "OSH", ca_device_type=ca_device_type, ca_bucket=ca_bucket,
+#                        ca_config=ca_config, ca_interface=ca_interface, ca_location=ca_location)
+
+
+
+"----------------------Пример лаконичной работы с циклом и функциями------------------------"
+# tasks = [(task, "One", work_queue), (task, "Two", work_queue)]
+# for t, n, q in tasks:
+#     t(n, q)
+
+
+
+"----------------------Подсчет маски----------------------------"
+# Из 255.255.255.0 >>> 24
+# # №1
+# def number_of_set_bits(x):
+#     x -= (x >> 1) & 0x55555555
+#     x = ((x >> 2) & 0x33333333) + (x & 0x33333333)
+#     x = ((x >> 4) + x) & 0x0f0f0f0f
+#     x += x >> 8
+#     x += x >> 16
+#     return x & 0x0000003f
+#
+# # №2
+# def number_of_set_bits(x):
+#     n = 0
+#     while x:
+#         n += x & 1
+#         x = x >> 1
+#     return n
+
+
+"----------------------Функция наполнения КЕ----------------------------"
+# # Функция наполнения КЕ - "installed_software"
+# def setting_attr(instSoftOSH, soft_attr):
+#     name = soft_attr.get('Name')
+#     value = soft_attr.get('Value')
+#
+#     if value == '' or name == 'Global_id':
+#         return instSoftOSH
+#
+#     # Получаем тип атрибута из XML
+#     attr_type = soft_attr.get('Type').split('.')[-1]
+#
+#     # Добавление атрибутов
+#     if attr_type == 'String':
+#         instSoftOSH.setStringAttribute(name, value)
+#     elif attr_type == 'Integer':
+#         value = value.replace(',', '')
+#         instSoftOSH.setIntegerAttribute(name, int(value))
+#     elif attr_type == 'Boolean':
+#         if value == 'True':
+#             value = True
+#             instSoftOSH.setBoolAttribute(name, value)
+#         else:
+#             value = False
+#             instSoftOSH.setBoolAttribute(name, value)
+#     elif attr_type == 'Date':
+#         instSoftOSH.setDateAttribute(name, value)
+#     elif attr_type == 'StringList':
+#         # setListAttribute or addAttributeToList but first want java list to input, second need already exist list?
+#         instSoftOSH.setListAttribute(name, [value])
+#     return instSoftOSH
+
+
+
+
+
+"-----------Функция порционной отправки данных в ucmdb-------------"
+# def sendObjectsIntoUcmdb(Framework, OSHVResult):
+#     for i in range(0, OSHVResult.size(), 15000):
+#         limit = i + 15000
+#         if limit >= OSHVResult.size():
+#             limit = OSHVResult.size()
+#
+#         vector = OSHVResult.getSubVector(i, limit)
+#         Framework.sendObjects(vector)
+#         Framework.flushObjects()
+#         vector.clear()
+
+"----------------------Обработка ошибок----------------------------"
+# if output.find('is not recognized') != -1:
+#     errormsg = "NMAP is not installed on Probe machine, or please check the nmap location is configured correctly."
+#     logger.error(errormsg)
+#     raise ValueError, errormsg
+
+
 "-------------------Действия с resultVector------------------------"
 # for j in resultVector:
 #     root_class = j.getAttributeAll()
@@ -97,3 +207,45 @@ p = None
 
 "------------------------Работа с CMD--------------------------"
 # client.executeCmd('ss -t4n state listening')
+
+"-------------------------Редактирование строки (убираются ненужные символы по индексу)----------------------"
+# def replace_char_at_index(org_str, index, replacement):
+#     new_str = org_str
+#     if index < len(org_str):
+#         new_str = org_str[0:index] + replacement + org_str[index + 1:]
+#     return new_str
+#
+#
+# def ip_address_debug_dot(string_ip):
+#     list_of_indexes = []
+#
+#     for i in range(len(string_ip) - 1):
+#         if string_ip[i] == "/" and string_ip[i + 1] == ".":
+#             list_of_indexes.append(i + 1)
+#
+#     for i in range(len(list_of_indexes)):
+#         if i != 0:
+#             string_ip = replace_char_at_index(string_ip, list_of_indexes[i] - i, '')
+#         else:
+#             string_ip = replace_char_at_index(string_ip, list_of_indexes[i], '')
+#
+#     return string_ip
+
+# # Эта функция одноразовая (для одного "случая"). Пока не стал делать универсальной
+# def ip_address_debug_bracket(string_ip):
+#     list_of_indexes = []
+#
+#     for i in range(len(string_ip) - 1):
+#         if (string_ip[i].isdigit() and string_ip[i + 1] == "(") or (string_ip[i].isdigit() and string_ip[i + 1] == ")"):
+#             list_of_indexes.append(i + 1)
+#
+#     for i in range(len(list_of_indexes)):
+#         if i != len(list_of_indexes)-1:
+#             if i != 0:
+#                 string_ip = replace_char_at_index(string_ip, list_of_indexes[i], '/')
+#             else:
+#                 string_ip = replace_char_at_index(string_ip, list_of_indexes[i], '/')
+#         else:
+#             string_ip = replace_char_at_index(string_ip, list_of_indexes[i], '')
+#
+#     return string_ip
